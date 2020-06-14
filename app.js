9d88cc535676bfeddd603195f30c8f23be26ba5b
RajatSaxena <saxenarajat26@gmail.com>
2020-06-15 02:33:53 +0530

    ready for heroku

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express=require("express")
 const app=express();
-
+const PORT=process.env.PORT || 3000;
 app.use(express.static("./public"));
 
 const fs = require('fs');
@@ -35,6 +35,6 @@ app.get("/economy",(req,res)=>{
 })
 
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log("server is up and running !! please check http://localhost:3000/")
 })
