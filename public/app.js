24644f624c78353ccc005d788bb5073607e418a6
RajatSaxena <saxenarajat26@gmail.com>
2020-06-15 03:58:45 +0530

    deleted the extra economy function in app.js

--- public/app.js
+++ public/app.js
@@ -9,7 +9,7 @@ fetchAndVisualizeData();
 function visualizeData(data) {
     visualiseMatchesPlayedPerYear(data.matchesplayedperyear);
     visualizeMatchesWonPerTeam(data.matchesWonPerTeam);
-    //visualizeEconomicalPlayer(data);
+
     visualizeWinningTeamPerVenue(data.winningTeamPerVenue);
     visualizeWinningTeamPerTeamPerSeason(data.winsPerTeamPerSeason);
     visualizeMostMatchesWon(data.mostMatchesWon);
@@ -37,13 +37,13 @@ function visualiseMatchesPlayedPerYear(MatchesPlayedPerYear) {
         },
     
         xAxis: {
-            categories:Object.keys(MatchesPlayedPerYear) //['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            categories:Object.keys(MatchesPlayedPerYear)
         },
     
         series: [{
             type: 'column',
             colorByPoint: true,
-            data: Object.values(MatchesPlayedPerYear),//[29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+            data: Object.values(MatchesPlayedPerYear),
             showInLegend: false
         }]
     
@@ -162,86 +162,7 @@ function visualizeMatchesWonPerTeam(matchesWonPerTeam) {
     // .then((data) => console.log(data.catch)).catch(e => console.log(e));
 }
 
-function visualizeEconomicalPlayer(data) {
-    const div = document.getElementById("economical-bowler")
-    div.addEventListener("mouseover", function () {
-        div.style.backgroundColor = "blue"
-    })
-    div.addEventListener("mouseout", function () {
-        div.style.backgroundColor = "white"
-    })
 
-    Highcharts.chart("economical-bowler", {
-        chart: {
-            type: 'column'
-        },
-        title: {
-            text: '4. Economical player 2017'
-        },
-        subtitle: {
-            text: 'Source: <a href="https://www.kaggle.com/nowke9/ipldata/data?select=matches.csv" target="_blank">IPL Dataset</a>'
-        },
-        xAxis: {
-            type: 'category',
-            labels: {
-                rotation: -45,
-                style: {
-                    fontSize: '13px',
-                    fontFamily: 'Verdana, sans-serif'
-                }
-            }
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: 'Population (millions)'
-            }
-        },
-        legend: {
-            enabled: false
-        },
-        tooltip: {
-            pointFormat: 'Population in 2017: <b>{point.y:.1f} millions</b>'
-        },
-        series: [{
-            name: 'Population',
-            data: [
-                ['Shanghai', 24.2],
-                ['Beijing', 20.8],
-                ['Karachi', 14.9],
-                ['Shenzhen', 13.7],
-                ['Guangzhou', 13.1],
-                ['Istanbul', 12.7],
-                ['Mumbai', 12.4],
-                ['Moscow', 12.2],
-                ['São Paulo', 12.0],
-                ['Delhi', 11.7],
-                ['Kinshasa', 11.5],
-                ['Tianjin', 11.2],
-                ['Lahore', 11.1],
-                ['Jakarta', 10.6],
-                ['Dongguan', 10.6],
-                ['Lagos', 10.6],
-                ['Bengaluru', 10.3],
-                ['Seoul', 9.8],
-                ['Foshan', 9.3],
-                ['Tokyo', 9.3]
-            ],
-            dataLabels: {
-                enabled: true,
-                rotation: -90,
-                color: '#FFFFFF',
-                align: 'right',
-                format: '{point.y:.1f}', // one decimal
-                y: 10, // 10 pixels down from the top
-                style: {
-                    fontSize: '13px',
-                    fontFamily: 'Verdana, sans-serif'
-                }
-            }
-        }]
-    });
-}
 
 
 function visualizeWinningTeamPerVenue(winningTeamPerVenue) {
