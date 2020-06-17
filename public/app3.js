37066ed284866d35ab176ec563a0e26fe14dea23
RajatSaxena <saxenarajat26@gmail.com>
2020-06-17 23:48:11 +0530

    only server chs

--- public/app3.js
+++ public/app3.js
@@ -13,7 +13,7 @@ function show(){
 
     // fetch("http://localhost:3000/extra-runs?season="+year)
     fetch('/extra-runs?season=' + year)
-    .then((resp)=>resp.json())
+    .then((resp)=>jSON.parse(resp))
     .then(respo => {
         visualizeData(respo[year]);
     function visualizeData(data){
