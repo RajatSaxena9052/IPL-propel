ee742fbd8e4f6080e880ba8bb4716737ee14ea9c
RajatSaxena <saxenarajat26@gmail.com>
2020-06-18 21:52:00 +0530

    2

--- server.js
+++ server.js
@@ -1,39 +1,39 @@
-const express=require("express")
-const app=express();
-let PORT=process.env.PORT || 3000;
+const express = require("express")
+const app = express();
+let PORT = process.env.PORT || 3000;
 
 app.use(express.static("./public"));
 
 const fs = require('fs');
 let data = JSON.parse(fs.readFileSync('./public/data.json', 'utf-8'));
+let extraData = { ...data }
 
-app.get("/extra",(req,res)=>{
-    if(!req.query.season){
+app.get("/extra",async (req, res) => {
+    if (!req.query.season) {
         return res.send(
             {
-                error:"this is not the season"
-            })  
+                error: "this is not the season"
+            })
     }
-    res.send(
-        console.log(data),
+    return res.status(200).send(
         res.json(data.extraRunsPerTeam2016)
     )
 })
 
-app.get("/economy",(req,res)=>{
-    if(!req.query.season1){
+app.get("/economy", async (req, res) => {
+    if (!req.query.season1) {
         return res.send(
             {
-                error:"this is not the season"
-            })  
+                error: "this is not the season"
+            })
     }
-    
-    res.send(
+
+   return res.send(
         res.json(data.economicalBowler2015)
-)
+    )
 })
 
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log("server is up and running !! please check http://localhost:3000/")
 })
