0b52673d848a4423a7ef47583bcaf06e34d432af
RajatSaxena <saxenarajat26@gmail.com>
2020-06-18 22:23:43 +0530

    2

--- public/app3.js
+++ public/app3.js
@@ -1,90 +1,79 @@
-function show() {
-    const div = document.getElementById("extra-Runs")
-    div.addEventListener("mouseover", function () {
-        div.style.backgroundColor = "blue"
-    })
-    div.addEventListener("mouseout", function () {
-        div.style.backgroundColor = "white"
-    })
-
+function show(){
+    const div=document.getElementById("extra-Runs")
+    div.addEventListener("mouseover",function(){
+        div.style.backgroundColor="blue"})
+    div.addEventListener("mouseout",function(){
+        div.style.backgroundColor="white"})
+      
     const year = document.getElementById("season").value;
 
-    if (year == "") {
-        alert("please select SEASON from the drop down list ")
+    if(year ==""){
+    alert("please select SEASON from the drop down list ")
     }
 
     // fetch("http://localhost:3000/extra-runs?season="+year)
-    fetch('/extra?season=' + year)
-        .then((resp) => {
-            console.log("response", typeof resp, resp)
-            resp.json()
-        })
-        .then(resp => {
-            // console.log("response", typeof resp, resp.body)
-            // console.log("response", typeof resp, JSON.stringify(resp))
-            // console.log("response", typeof resp, JSON.parse(resp))
-
-            visualizeData(resp[year])
-            function visualizeData(data) {
-                console.log(data, "from client side")
-
-
-                let a = [];
-                for (let i in data) {
-                    a.push([i, data[i]])
-                }
-
-                Highcharts.chart("extra-Runs", {
-                    chart: {
-                        type: 'column'
-                    },
-                    title: {
-                        text: `3. Extra runs conceded by each team in ${year}`
-                    },
-                    subtitle: {
-                        text: 'Source: <a href="https://www.kaggle.com/nowke9/ipldata/data?select=matches.csv" target="_blank">IPL Dataset</a>'
-                    },
-                    xAxis: {
-                        type: 'category',
-                        labels: {
-                            rotation: -45,
-                            style: {
-                                fontSize: '13px',
-                                fontFamily: 'Verdana, sans-serif'
-                            }
-                        }
-                    },
-                    yAxis: {
-                        min: 0,
-                        title: {
-                            text: 'Extra Runs'
-                        }
-                    },
-                    legend: {
-                        enabled: false
-                    },
-                    tooltip: {
-                        pointFormat: 'Extra Runs: <b>{point.y:.0f}</b>'
-                    },
-                    series: [{
-                        name: 'Extra Runs',
-                        data: a,
-                        dataLabels: {
-                            enabled: true,
-                            rotation: -90,
-                            color: '#FFFFFF',
-                            align: 'right',
-                            format: '{point.y:.0f}', // one decimal
-                            y: 10, // 10 pixels down from the top
-                            style: {
-                                fontSize: '13px',
-                                fontFamily: 'Verdana, sans-serif'
-                            }
-                        }
-                    }]
-                });
+    fetch('/extra')
+    .then((resp)=>resp.json())
+    .then(resp => {
+        visualizeData(resp[year]);
+    function visualizeData(data){
+        console.log(data,"from client side")
+        
+                let a=[];
+            for(let i in data){
+                a.push([i,data[i]])
             }
-        }).catch(err => {
-            console.error('Error: ', err);
-        })
+
+    Highcharts.chart("extra-Runs", {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: `3. Extra runs conceded by each team in ${year}`
+    },
+    subtitle: {
+        text: 'Source: <a href="https://www.kaggle.com/nowke9/ipldata/data?select=matches.csv" target="_blank">IPL Dataset</a>'
+    },
+    xAxis: {
+        type: 'category',
+        labels: {
+            rotation: -45,
+            style: {
+                fontSize: '13px',
+                fontFamily: 'Verdana, sans-serif'
+            }
+        }
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Extra Runs'
+        }
+    },
+    legend: {
+        enabled: false
+    },
+    tooltip: {
+        pointFormat: 'Extra Runs: <b>{point.y:.0f}</b>'
+    },
+    series: [{
+        name: 'Extra Runs',
+        data:a,
+        dataLabels: {
+            enabled: true,
+            rotation: -90,
+            color: '#FFFFFF',
+            align: 'right',
+            format: '{point.y:.0f}', // one decimal
+            y: 10, // 10 pixels down from the top
+            style: {
+                fontSize: '13px',
+                fontFamily: 'Verdana, sans-serif'
+            }
+        }
+    }]
+});
+}
+}).catch(err => {
+    console.error('Error: ', err);})
 }
