87d71221e02593e0a7d056fe887cd615de9f464a
RajatSaxena <saxenarajat26@gmail.com>
2020-06-20 01:24:27 +0530

    heelollo

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express();
 let PORT = process.env.PORT || 3000;
 
 app.use(express.static("./public"));
-app.use(express.json("/"))
+app.use(express.json())
 const fs = require('fs');
 let data = JSON.parse(fs.readFileSync('./public/data.json', 'utf-8'));
-let extraData = { ...data }
+let extraData = { ...data}
 
 app.get("/extra/:year", (req, res) => {
     
