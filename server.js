403cd92d27f05cc43af8a7ce3f43f4264143ff3f
RajatSaxena <saxenarajat26@gmail.com>
2020-06-20 01:30:22 +0530

    heelollo

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const app = express();
 let PORT = process.env.PORT || 3000;
 
 app.use(express.static("./public"));
-app.use(express.json())
+app.use(express.json("/"))
 const fs = require('fs');
 let data = JSON.parse(fs.readFileSync('./public/data.json'));
-let extraData = { ...data}
+let extraData = { ...data }
 
 app.get("/extra/:year", (req, res) => {
     
    let year = req.params.year;
 
-    res.send(extraData.extraRunsPerTeam2016[year])
+    res.send(res.json(data.extraRunsPerTeam2016[year]))
 
     // if (!req.query.season) {
     //     return res.send(
